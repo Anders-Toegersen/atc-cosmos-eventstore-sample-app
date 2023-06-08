8c35cb779506e929236fca58d8d2c001489ff0fa
Anders Tøgersen (Delegate) <ato@delegate.dk>
2023-06-08 12:52:57 +0200

    WIP 234

--- sample/Sample.ExpenseTracker/Endpoints/ManagementEndpoints.cs
+++ sample/Sample.ExpenseTracker/Endpoints/ManagementEndpoints.cs
@@ -12,81 +12,123 @@ public static class ManagementEndpoints
 {
     public static void MapEndpoints(WebApplication app)
     {
-        app.MapPost(
-            "users/register",
-            async (
-                [FromBody] UserRequest request,
-                [FromServices] ICommandProcessor<RegisterUserCommand> processor,
-                CancellationToken cancellationToken)
-                =>
+        app.MapPost("users/register", RegisterUser)
+            .WithSummary(nameof(RegisterUser))
+            .WithTags("Management")
+            .WithOpenApi();
+
+        app.MapGet("users/{userId}", DisplayUser)
+            .WithSummary(nameof(DisplayUser))
+            .WithTags("Management")
+            .WithOpenApi();
+
+        app.MapGet("/expenses/list/{status}", DisplayAllExpensesByStatus)
+            .WithSummary(nameof(DisplayAllExpensesByStatus))
+            .WithTags("Management")
+            .WithOpenApi();
+
+        app.MapPost("users/{userId}/expenses/{expenseId}/approve", ApproveExpenseForUserById)
+            .WithSummary(nameof(ApproveExpenseForUserById))
+            .WithTags("Management")
+            .WithOpenApi();
+
+        app.MapPost("users/{userId}/expenses/{expenseId}/reject", RejectExpenseForUserById)
+            .WithSummary(nameof(RejectExpenseForUserById))
+            .WithTags("Management")
+            .WithOpenApi();
+    }
+
+    private static async Task<IResult> RegisterUser(
+        [FromBody] UserRequest request,
+        [FromServices] ICommandProcessor<RegisterUserCommand> processor,
+        CancellationToken cancellationToken)
+    {
+        var commandResult = await processor.ExecuteAsync(
+            new RegisterUserCommand(
+                                Guid.NewGuid(),
+                                request.Username,
+                                request.Email),
+                            cancellationToken);
+
+        return commandResult switch
+        {
             {
-                var commandResult = await processor.ExecuteAsync(
-                    new RegisterUserCommand(
-                        Guid.NewGuid(),
-                        request.Username,
-                        request.Email),
-                    cancellationToken);
+                Result: ResultType.Changed, Response: { } response
+            } => Results.Json(response, statusCode: 201),
+            { Result: ResultType.Exists } => Results.Conflict("User already exists"),
+            _ => Results.Problem("Unexpected error occured"),
+        };
+    }
 
-                return commandResult switch
-                {
-                    { Result: ResultType.Changed, Response: { } response } => Results.Json(response, statusCode: 201),
-                    { Result: ResultType.Exists } => Results.Conflict("User already exists"),
-                    _ => Results.Problem("Unexpected error occured"),
-                };
-            })
-            .WithTags("Management");
+    private static async Task<IResult> DisplayUser(
+        [FromHeader] Guid userId,
+        [FromServices] ICosmosReader<ExpenseResource> reader,
+        CancellationToken cancellationToken)
+    {
+        var queryResult = await reader.FindAsync(
+            userId.ToString(),
+            ExpenseResource.PartitionKey,
+            cancellationToken);
 
-        app.MapGet(
-            "users/{userId}",
-            async (
-                [FromHeader] Guid userId,
-                [FromServices] ICosmosReader<ExpenseResource> reader,
-                CancellationToken cancellationToken)
-                =>
-            {
-                var queryResult = await reader.FindAsync(
-                    userId.ToString(),
-                    ExpenseResource.PartitionKey,
-                    cancellationToken);
+        return queryResult switch
+        {
+            { View: { } view } => Results.Json(view, statusCode: 200),
+            _ => Results.NotFound("User not found"),
+        };
+    }
 
-                return queryResult switch
-                {
-                    { View: { } view } => Results.Json(view, statusCode: 200),
-                    _ => Results.NotFound("User not found"),
-                };
-            })
-            .WithTags("Management");
+    private static async Task<IResult> DisplayAllExpensesByStatus(
+        [FromRoute] Status status,
+        [FromServices] ICosmosReader<ExpenseResource> reader,
+        CancellationToken cancellationToken)
+    {
+        var query = new QueryDefinition("""
+                    SELECT c.id as userId, t AS expense
+                    FROM c
+                    JOIN t IN c.view.expenses
+                    WHERE t.status = @Status
+                    """)
+        .WithParameter("@Status", status);
 
-        app.MapPost(
-            "users/{userId}/expenses/{expenseId}/approve",
-            async (
-                [FromRoute] Guid userId,
-                [FromRoute] Guid expenseId,
-                [FromServices] ICommandProcessor<ApproveExpenseCommand> command,
-                CancellationToken cancellationToken)
-                => await command
+        var queryResult = reader.QueryAsync<UserExpense>(
+            query,
+            ExpenseResource.PartitionKey,
+            cancellationToken);
+
+        var expensesList = new List<UserExpense>();
+        await foreach (var expense in queryResult.WithCancellation(cancellationToken))
+        {
+            expensesList.Add(expense);
+        }
+
+        return Results.Json(expensesList, statusCode: 200);
+    }
+
+    private static async Task<IResult> ApproveExpenseForUserById(
+        [FromRoute] Guid userId,
+        [FromRoute] Guid expenseId,
+        [FromServices] ICommandProcessor<ApproveExpenseCommand> command,
+        CancellationToken cancellationToken)
+        => await command
             .ExecuteAsync(
                 new ApproveExpenseCommand(
                     userId,
                     expenseId),
                 cancellationToken)
             switch
-                {
-                    { Result: ResultType.Changed } => Results.Ok(),
-                    { Result: ResultType.NotModified } => Results.NotFound(),
-                    _ => Results.Problem("Unexpected error occured"),
-                })
-        .WithTags("Management");
+        {
+            { Result: ResultType.Changed } => Results.Ok(),
+            { Result: ResultType.NotModified } => Results.NotFound(),
+            _ => Results.Problem("Unexpected error occured"),
+        };
 
-        app.MapPost(
-            "users/{userId}/expenses/{expenseId}/reject",
-            async (
-                [FromRoute] Guid userId,
-                [FromRoute] Guid expenseId,
-                [FromBody] string? reason,
-                [FromServices] ICommandProcessor<RejectExpenseCommand> command,
-                CancellationToken cancellationToken)
-                => await command
+    private static async Task<IResult> RejectExpenseForUserById(
+        [FromRoute] Guid userId,
+        [FromRoute] Guid expenseId,
+        [FromBody] string? reason,
+        [FromServices] ICommandProcessor<RejectExpenseCommand> command,
+        CancellationToken cancellationToken)
+        => await command
             .ExecuteAsync(
                 new RejectExpenseCommand(
                     userId,
@@ -94,42 +136,9 @@ public static class ManagementEndpoints
                     reason),
                 cancellationToken)
             switch
-                {
-                    { Result: ResultType.Changed } => Results.Ok(),
-                    { Result: ResultType.NotModified } => Results.NotFound(),
-                    _ => Results.Problem("Unexpected error occured"),
-                })
-        .WithTags("Management");
-
-        app.MapGet(
-            "/expenses/{status}",
-            async (
-                [FromRoute] Status status,
-                [FromServices] ICosmosReader<ExpenseResource> reader,
-                CancellationToken cancellationToken)
-                =>
-            {
-                var query = new QueryDefinition("""
-                    SELECT c.id as userId, t AS expense
-                    FROM c
-                    JOIN t IN c.view.expenses
-                    WHERE t.status = @Status
-                    """)
-                .WithParameter("@Status", status);
-
-                var queryResult = reader.QueryAsync<UserExpense>(
-                    query,
-                    ExpenseResource.PartitionKey,
-                    cancellationToken);
-
-                var expensesList = new List<UserExpense>();
-                await foreach (var expense in queryResult.WithCancellation(cancellationToken))
-                {
-                    expensesList.Add(expense);
-                }
-
-                return Results.Json(expensesList, statusCode: 200);
-            })
-        .WithTags("Management");
-    }
+        {
+            { Result: ResultType.Changed } => Results.Ok(),
+            { Result: ResultType.NotModified } => Results.NotFound(),
+            _ => Results.Problem("Unexpected error occured"),
+        };
 }
