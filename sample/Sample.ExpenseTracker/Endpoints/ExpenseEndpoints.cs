05b07ef1062b14d932080ec9a254a3290d25b92f
Anders Tøgersen (Delegate) <ato@delegate.dk>
2023-06-08 13:59:01 +0200

    Added summaries for ExpenseEndpoints

--- sample/Sample.ExpenseTracker/Endpoints/ExpenseEndpoints.cs
+++ sample/Sample.ExpenseTracker/Endpoints/ExpenseEndpoints.cs
@@ -11,109 +11,113 @@ public static class ExpenseEndpoints
 {
     public static void MapEndpoints(WebApplication app)
     {
-        app.MapGet(
-            "/expenses/{expenseId}",
-            async (
-                [FromHeader] Guid userId,
-                [FromRoute] Guid expenseId,
-                [FromServices] ICosmosReader<ExpenseResource> reader,
-                CancellationToken cancellationToken)
-                =>
-            {
-                var queryResult = await reader.FindAsync(
-                    userId.ToString(),
-                    ExpenseResource.PartitionKey,
-                    cancellationToken);
+        app.MapGet("/expenses/{expenseId}", DisplayExpense)
+            .WithSummary(nameof(DisplayExpense))
+            .WithTags("Expense")
+            .WithOpenApi();
 
-                var expenseQuery = queryResult?.View.Expenses.SingleOrDefault(e => e.Id == expenseId.ToString());
+        app.MapPost("/expenses", CreateExpense)
+            .WithSummary(nameof(CreateExpense))
+            .WithTags("Expense")
+            .WithOpenApi();
 
-                return expenseQuery switch
-                {
-                    { } expense => Results.Json(expense, statusCode: 200),
-                    _ => Results.NotFound("User not found"),
-                };
-            })
-            .WithTags("Expense");
+        app.MapPut("/expenses/{expenseId}", UpdateExpense)
+            .WithSummary(nameof(UpdateExpense))
+            .WithTags("Expense")
+            .WithOpenApi();
 
-        app.MapPost(
-            "/expenses",
-            async (
+        app.MapDelete("/expenses/{expenseId}", DeleteExpense)
+            .WithSummary(nameof(DeleteExpense))
+            .WithTags("Expense")
+            .WithOpenApi();
+    }
+
+    private static async Task<IResult> DisplayExpense(
+        [FromHeader] Guid userId,
+        [FromRoute] Guid expenseId,
+        [FromServices] ICosmosReader<ExpenseResource> reader,
+        CancellationToken cancellationToken)
+    {
+        var queryResult = await reader.FindAsync(
+            userId.ToString(),
+            ExpenseResource.PartitionKey,
+            cancellationToken);
+
+        var expenseQuery = queryResult?.View.Expenses.SingleOrDefault(e => e.Id == expenseId.ToString());
+
+        return expenseQuery switch
+        {
+            { } expense => Results.Json(expense, statusCode: 200),
+            _ => Results.NotFound("User not found"),
+        };
+    }
+
+    private static async Task<IResult> CreateExpense(
                 [FromHeader] Guid userId,
                 [FromBody] ExpenseRequest request,
                 [FromServices] ICommandProcessor<RecordExpenseCommand> processor,
                 CancellationToken cancellationToken)
-                =>
-            {
-                var commandResult = await processor.ExecuteAsync(
-                    new RecordExpenseCommand(
-                        userId,
-                        Guid.NewGuid(),
-                        request.Description,
-                        request.Amount,
-                        request.Category,
-                        request.Timestamp),
-                    cancellationToken);
+    {
+        var commandResult = await processor.ExecuteAsync(
+            new RecordExpenseCommand(
+                userId,
+                Guid.NewGuid(),
+                request.Description,
+                request.Amount,
+                request.Category,
+                request.Timestamp),
+            cancellationToken);
 
-                return commandResult switch
-                {
-                    { Result: ResultType.Changed, Response: { } response } => Results.Json(response, statusCode: 201),
-                    { Result: ResultType.NotFound } => Results.NotFound("User does not exist"),
-                    _ => Results.Problem("Unexpected error occured"),
-                };
-            })
-            .WithTags("Expense");
+        return commandResult switch
+        {
+            { Result: ResultType.Changed, Response: { } response } => Results.Json(response, statusCode: 201),
+            { Result: ResultType.NotFound } => Results.NotFound("User does not exist"),
+            _ => Results.Problem("Unexpected error occured"),
+        };
+    }
 
-        app.MapPut(
-            "/expenses/{expenseId}",
-            async (
-                [FromHeader] Guid userId,
-                [FromRoute] Guid expenseId,
-                [FromBody] ExpenseRequest request,
-                [FromServices] ICommandProcessor<UpdateExpenseCommand> processor,
-                CancellationToken cancellationToken)
-                =>
-            {
-                var commandResult = await processor.ExecuteAsync(
-                    new UpdateExpenseCommand(
-                        userId,
-                        expenseId,
-                        request.Description,
-                        request.Amount,
-                        request.Category,
-                        request.Timestamp),
-                    cancellationToken);
+    private static async Task<IResult> UpdateExpense(
+        [FromHeader] Guid userId,
+        [FromRoute] Guid expenseId,
+        [FromBody] ExpenseRequest request,
+        [FromServices] ICommandProcessor<UpdateExpenseCommand> processor,
+        CancellationToken cancellationToken)
+    {
+        var commandResult = await processor.ExecuteAsync(
+            new UpdateExpenseCommand(
+                userId,
+                expenseId,
+                request.Description,
+                request.Amount,
+                request.Category,
+                request.Timestamp),
+            cancellationToken);
 
-                return commandResult switch
-                {
-                    { Result: ResultType.Changed, Response: { } response } => Results.Json(response, statusCode: 200),
-                    { Result: ResultType.NotModified, Response: { } response } => Results.NotFound(response),
-                    _ => Results.Problem("Unexpected error occured"),
-                };
-            })
-            .WithTags("Expense");
+        return commandResult switch
+        {
+            { Result: ResultType.Changed, Response: { } response } => Results.Json(response, statusCode: 200),
+            { Result: ResultType.NotModified, Response: { } response } => Results.NotFound(response),
+            _ => Results.Problem("Unexpected error occured"),
+        };
+    }
 
-        app.MapDelete(
-            "/expenses/{expenseId}",
-            async (
-                [FromHeader] Guid userId,
-                [FromRoute] Guid expenseId,
-                [FromServices] ICommandProcessor<DeleteExpenseCommand> processor,
-                CancellationToken cancellationToken)
-                =>
-            {
-                var commandResult = await processor.ExecuteAsync(
-                    new DeleteExpenseCommand(
-                        userId,
-                        expenseId),
-                    cancellationToken);
+    private static async Task<IResult> DeleteExpense(
+        [FromHeader] Guid userId,
+        [FromRoute] Guid expenseId,
+        [FromServices] ICommandProcessor<DeleteExpenseCommand> processor,
+        CancellationToken cancellationToken)
+    {
+        var commandResult = await processor.ExecuteAsync(
+            new DeleteExpenseCommand(
+                userId,
+                expenseId),
+            cancellationToken);
 
-                return commandResult switch
-                {
-                    { Result: ResultType.Changed, Response: { } response } => Results.Json(response, statusCode: 200),
-                    { Result: ResultType.NotModified, Response: { } response } => Results.NotFound(response),
-                    _ => Results.Problem("Unexpected error occured"),
-                };
-            })
-            .WithTags("Expense");
+        return commandResult switch
+        {
+            { Result: ResultType.Changed, Response: { } response } => Results.Json(response, statusCode: 200),
+            { Result: ResultType.NotModified, Response: { } response } => Results.NotFound(response),
+            _ => Results.Problem("Unexpected error occured"),
+        };
     }
 }
